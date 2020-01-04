6ed7bd84348638d2d3b97f9aac60aedf6f78e11d
Stanislav <st4s1k24@gmail.com>
2020-01-04 11:44:22 +0200

    disable host checking for testing purposes

--- src/main/java/com/st4s1k/charlie/SSHManager.java
+++ src/main/java/com/st4s1k/charlie/SSHManager.java
@@ -86,7 +86,7 @@ public class SSHManager {
           strConnectionIP, intConnectionPort);
       sesConnection.setPassword(strPassword);
       // UNCOMMENT THIS FOR TESTING PURPOSES, BUT DO NOT USE IN PRODUCTION
-      // sesConnection.setConfig("StrictHostKeyChecking", "no");
+      sesConnection.setConfig("StrictHostKeyChecking", "no");
       sesConnection.connect(intTimeOut);
     } catch (JSchException jschX) {
       errorMessage = jschX.getMessage();
