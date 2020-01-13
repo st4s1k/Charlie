e82a8a08548538585751e9aec30b47009cc5c51a
Stanislav <st4s1k24@gmail.com>
2020-01-13 20:54:06 +0200

    refactoring

--- src/main/java/com/st4s1k/charlie/service/CharlieService.java
+++ src/main/java/com/st4s1k/charlie/service/CharlieService.java
@@ -106,7 +106,7 @@ public class CharlieService {
       final String hostInfo,
       final ChatSession chatSession) {
     final var userNameRegex = "[A-Za-z0-9\\-.]+";
-    final var hostNameRegex = "[a-z_][a-z0-9_\\-]*[$]?";
+    final var hostNameRegex = "^\\d+{1,3}\\.\\d+{1,3}\\.\\d+{1,3}\\.\\d+{1,3}$";
     final var portRegex = "^" +
         "([0-9]{1,4}" +
         "|[1-5][0-9]{4}" +
